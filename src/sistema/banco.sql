7963237d5ca3149c05af82dc954b15e7b04896b2
Jonatas <jonatas.silvestrini@gmail.com>
2018-08-24 22:40:18 -0300

    First Commit

---
+++ src/sistema/banco.sql
@@ -0,0 +1,42 @@
+drop database if exists banco;
+create database banco;
+
+use banco;
+
+create table clientes(
+	codigo int auto_increment,
+	nome varchar(50),
+	fone varchar(15),
+	data_nasc date,
+	primary key (codigo)
+);
+
+create table viagens(
+	codigo int auto_increment,
+	descricao varchar(50),
+	preco float,
+	foto date,
+	primary key (codigo)
+);
+
+create table usuarios(
+	codigo int auto_increment,
+	nome varchar(50),
+	senha varchar(15),
+	primary key (codigo)
+);
+
+create table vendas(
+	codtur int auto_increment,
+	codcli int,
+	codvi int,
+	data_saida date,
+	data_chegada date,
+	n_pessoas int,
+	valor float,
+	primary key (codtur)
+);
+
+alter table vendas add foreign key (codcli) references clientes (codigo);
+
+alter table vendas add foreign key (codvi) references viagens (codigo);
